eaecbf7364da8de8625c923999b049546e8dd2d1
Shist <shist.pupust@mail.ru>
2024-04-04 15:35:22 +0300

    Added basic structure of the project files

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import toggleFragment from "./modules/toggleFragment.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  toggleFragment();
+});
---
+++ scripts/modules/toggleFragment.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function toggleFragment() {
+  // Here will be logic for toggling two fragments on the web site
+}
+
+export default toggleFragment;
