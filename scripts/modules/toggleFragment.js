bc1360ae474caa5650958ebc24314bc999b77eb8
Shist <shist.pupust@mail.ru>
2024-04-04 16:23:29 +0300

    Added all needed svg icons

--- scripts/modules/toggleFragment.js
+++ scripts/modules/toggleFragment.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function toggleFragment() {
-  // Here will be logic for toggling two fragments on the web site
+  // Here will be logic for toggling two fragments on the web site (if needed)
 }
 
 export default toggleFragment;
